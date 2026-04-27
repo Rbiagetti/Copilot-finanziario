b71ed609c181d198711303835153c45cfd27b1ee
Roberto Biagetti <rbiagetti2001@gmail.com>
2026-04-27 21:46:47 +0200

    Fix React import in AppInitializer

--- frontend/src/AppInitializer.tsx
+++ frontend/src/AppInitializer.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useAuthStore } from "./store/authStore";
 
 export default function AppInitializer({ children }: { children: React.ReactNode }) {
